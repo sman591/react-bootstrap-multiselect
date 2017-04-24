45ad61fd498732ca5b42c02cf52af87d84023175
Stuart Olivera <stuart@stuartolivera.com>
2017-04-24 12:12:35 -0400

    Rebuild

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// setup jQuery
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -10,6 +8,12 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var React = require('react');
+var objectAssign = require('object-assign');
+var getOptions = require('./get-options.js');
+var bsMultiselect = require('./bootstrap-multiselect.js');
+var bsDropdown = require('./bootstrap-dropdown.js');
+// setup jQuery
 var $;
 if (typeof window !== 'undefined' && window && window.jQuery) {
 	$ = window.jQuery;
@@ -17,21 +21,6 @@ if (typeof window !== 'undefined' && window && window.jQuery) {
 	$ = require('jquery');
 	window.jQuery = $;
 }
-
-var BS = require('bootstrap');
-var React = require('react');
-var objectAssign = require('object-assign');
-var getOptions = require('./get-options.js');
-var bsMultiselect = require('./bootstrap-multiselect.js');
-var bsDropdown;
-
-// make it play nice when we already have bootstrap dropdown loaded.
-if (typeof BS === 'undefined' || typeof BS.dropdown === 'undefined') {
-	bsDropdown = require('./bootstrap-dropdown.js');
-} else {
-	bsDropdown = BS.dropdown;
-}
-
 $ = bsDropdown.init($);
 $ = bsMultiselect.init($);
 
